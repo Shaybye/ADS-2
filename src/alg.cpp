3045e90ea772e62944bff295b7b29161060ec6b6
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-03-09 18:00:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,21 +5,21 @@
 
 double pown(double value, uint16_t n) {
   if (n == 0)
-      return 1;
+    return 1;
   if (n == 1)
-      return value;
+    return value;
   return value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
   if (n == 1 || n == 0)
-      return 1;
+    return 1;
   return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
   if (x == 0)
-      return 0;
+    return 0;
   return (pown(x, n) / fact(n));
 }
 
@@ -33,14 +33,13 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double sin = x;
   for (int i = 2; i <= count; ++i) 
-      sin += pown(-1,i-1)* calcItem(x, i*2-1);
-
+    sin += pown(-1,i-1)* calcItem(x, i*2-1);
   return sin;
 }
 
 double cosn(double x, uint16_t count) {
   double cos = 1;
   for (int i = 2; i <= count; ++i)
-      cos += pown(-1, i - 1) * calcItem(x, i * 2 - 2);
+    cos += pown(-1, i - 1) * calcItem(x, i * 2 - 2);
   return cos;
 }
